e51db1f4575ccb60f341ce35ef898d2f8c4dce8f
ksawio97 <ksawery9708@gmail.com>
2023-12-08 20:19:06 +0100

    Completed part 2 of day 4 and added types comments

--- Day-4/func.js
+++ Day-4/func.js
@@ -1,7 +1,17 @@
 const fs = require('fs');
 
+/**
+ * 
+ * @param {string} path 
+ * @returns {[string]}
+ */
 const loadData = (path) => fs.readFileSync(path, 'utf-8').split('\n');
 
+/**
+ * 
+ * @param {[string]} loadData 
+ * @returns {[[string, string]]}
+ */
 const formatData = (loadData) => 
     loadData.map((line) => {                               
         const colonIndex = line.indexOf(':');                              
@@ -9,26 +19,79 @@ const formatData = (loadData) =>
         return line.slice(colonIndex + 2, line.length).split(' | ');                         
     });
 
+/**
+ * 
+ * @param {string} str 
+ * @returns {[string]}
+ */
 const getNumsArray = (str) => {
     const numsPattern = /\d+/g;
     return [...str.matchAll(numsPattern)].map((result) => result[0])
 }
 
+/**
+ * 
+ * @param {[string]} got 
+ * @param {[string]} pool 
+ * @returns {number}
+ */
+const countMatches = (got, pool) => {
+    let count = 0;
+    const poolSet = new Set(getNumsArray(pool));
+
+    getNumsArray(got).forEach((num) => {
+        if (poolSet.has(num))
+            count += 1;
+    })
+    return count;
+}
+
+/**
+ * 
+ * @param {string} path 
+ * @returns {number}
+ */
 exports.part1 = (path) => {
     const lines = formatData(loadData(path))
 
     let sum = 0
     lines.forEach(([got, pool]) => {
-        let count = -1;
-        const setPool = new Set(getNumsArray(pool));
-
-        getNumsArray(got).forEach((num) => {
-            if (setPool.has(num))
-                count += 1;
-        })
+        const count = countMatches(got, pool) - 1;
         if (count !== -1)
             sum += 2 ** count;
     });
 
     return sum;
+}
+
+/**
+ * 
+ * @param {string} path 
+ * @returns {number}
+ */
+exports.part2 = (path) => {
+    const lines = formatData(loadData(path))
+
+    const additionalCopies = new Map();
+    let copiesSum = 0;
+    lines.forEach(([got, pool], currIndex) => {
+        let count = countMatches(got, pool);
+        //get amount of cards that acts as multiplier
+        const multiplier = additionalCopies.get(currIndex) === undefined ? 1 : additionalCopies.get(currIndex) + 1;
+
+        //current element copies are no longer needed
+        additionalCopies.delete(currIndex);
+
+        //add copies to next cards iteration
+        while (count > 0) {
+            const key = currIndex + count;
+            const currentCount = additionalCopies.get(key);
+            additionalCopies.set(key, (currentCount === undefined ? 0 : currentCount) + 1 * multiplier);
+            count--;
+        }
+        //add copies to sum
+        copiesSum += multiplier;
+    });
+
+    return copiesSum;
 }
--- Day-4/index.js
+++ Day-4/index.js
@@ -2,3 +2,6 @@ const func = require('./func.js');
 
 console.log(func.part1('./test_input.txt'))
 console.log(func.part1('./input.txt'))
+
+console.log(func.part2('./test_input.txt'))
+console.log(func.part2('./input.txt'))
