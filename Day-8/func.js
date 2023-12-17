97df38e15133892f6bcde401f9f4c39e0060bb5f
ksawio97 <ksawery9708@gmail.com>
2023-12-17 23:28:54 +0100

    Day 8 part 2 test works but input.txt doesnt work

--- Day-8/func.js
+++ Day-8/func.js
@@ -10,9 +10,9 @@ const readFileData = (path) => fs.readFileSync(path, 'utf-8').split('\n');
 /**
  * 
  * @param {string[]} lines 
- *
+ * @returns {[number[], Map<string, [string, string]>]}
  */
-const parseData = (lines) => {
+const parseData1 = (lines) => {
     const instructions = [...lines.splice(0, 2)[0]].map((c) => c === 'L' ? 0 : 1);
 
     const lettersPattern = /[A-Z]+/g;
@@ -27,17 +27,76 @@ const parseData = (lines) => {
 /**
  * 
  * @param {string} path 
- * @returns 
+ * @returns {number}
  */
 exports.part1 = (path) => {
-    const [instructions, nodes] = parseData(readFileData(path));
+    const [instructions, nodes] = parseData1(readFileData(path));
 
     let currentNode = 'AAA';
     let stepsCount = 0;
-    while (currentNode != 'ZZZ')
+    while (currentNode !== 'ZZZ')
     {
         const option = instructions[stepsCount++ % instructions.length];
         currentNode = nodes.get(currentNode)[option];
+    }
+    return stepsCount;
+}
+
+/**
+ * 
+ * @param {string[]} lines 
+ * @returns {[number[], string[], string[], Map<string, [string, string]>]}
+ */
+const parseData2 = (lines) => {
+    const instructions = [...lines.splice(0, 2)[0]].map((c) => c === 'L' ? 0 : 1);
+
+    const nodesPattern = /[A-Z\d]+/g;
+
+    const startNodesKeys = [];
+    const endNodesKeys = [];
+    const nodes = new Map();
+    lines.forEach((line) => {
+        const letters = [...line.matchAll(nodesPattern)].map((item) => item[0]);
+
+        if (letters[0][2] === 'A')
+            startNodesKeys.push(letters[0]);
+        else if (letters[0][2] === 'Z')
+            endNodesKeys.push(letters[0]);
+
+        nodes.set(letters[0], letters.splice(1, 2));
+    });
+
+    return [instructions, startNodesKeys, endNodesKeys, nodes];
+}
+
+/**
+ * 
+ * @param {string[]} currentNodesKeys 
+ * @param {string[]} destinationNodesKeys 
+ * @returns {boolean}
+ */
+const allNodesReachedDestination = (currentNodesKeys, destinationNodesKeys) => {
+    for (const currentNodeKey of currentNodesKeys)
+        if (!(destinationNodesKeys.includes(currentNodeKey)))
+            return false;
+
+    return true;
+}
+
+/**
+ * 
+ * @param {string} path 
+ * @returns {number}
+ */
+exports.part2 = (path) => {
+    const [instructions, startNodesKeys, endNodesKeys, nodes] = parseData2(readFileData(path));
+
+    let stepsCount = 0;
+    for (; !allNodesReachedDestination(startNodesKeys, endNodesKeys); stepsCount++)
+    {       
+        const option = instructions[stepsCount % instructions.length];
+        for (let i = 0; i < startNodesKeys.length; i++)
+            startNodesKeys[i] = nodes.get(startNodesKeys[i])[option];
     }
     return stepsCount;
 }
--- Day-8/index.js
+++ Day-8/index.js
@@ -1,5 +1,9 @@
 const func = require('./func.js');
 
-console.log(func.part1('./test_input.txt'));
-console.log(func.part1('./test_input1.txt'));
-console.log(func.part1('./input.txt'));
+// console.log(func.part1('./test_input.txt'));
+// console.log(func.part1('./test_input1.txt'));
+// console.log(func.part1('./input.txt'));
+
+
+console.log(func.part2('./test_input2.txt'));
+console.log(func.part2('./input.txt'));
