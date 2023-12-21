bf8112eff62865590c9200452fa24320b236eb62
ksawio97 <ksawery9708@gmail.com>
2023-12-21 23:14:03 +0100

    Completed day 3 part 2

--- Day-3/func.js
+++ Day-3/func.js
@@ -95,4 +95,111 @@ const part1 = (path) => {
     return sum;
 }
 
-module.exports = {part1, checkIfNumIsValid}
+/**
+ * 
+ * @param {Array<string>} lines 
+ * @param {number} numStart 
+ * @param {number} numEnd 
+ * @param {number} row 
+ * @returns {[number, number]}
+ */
+const getSpecialNodePosition = (lines, numStart, numEnd, row) => {
+    //check node before number
+    if (numStart !== 0)
+    {
+        numStart -= 1;
+        if (isSymbol(lines[row][numStart]))
+            return [row, numStart];
+    }
+    
+    //check node after number
+    if (numEnd !== lines[row].length - 1)
+    {
+        numEnd += 1;
+        if (isSymbol(lines[row][numEnd]))
+            return [row, numEnd];
+    }
+
+    //check line above
+    if (row !== 0) {
+        const index = [...lines[row - 1].substring(numStart, numEnd + 1)].findIndex((char) => isSymbol(char));
+        if (index !== -1)
+            return [row - 1, numStart + index];
+    }
+
+    //check line below
+    if (row !== lines.length - 1) {
+        const index = [...lines[row + 1].substring(numStart, numEnd + 1)].findIndex((char) => isSymbol(char));
+        if (index !== -1)
+            return [row + 1, numStart + index];
+    }
+        
+    return [-1, -1];
+}
+
+/**
+ * 
+ * @param {Map<string, number>} multiplyPos 
+ * @param {Array<string>} lines 
+ * @param {number} numberStartCol 
+ * @param {number} numberEndCol 
+ * @param {number} row 
+ * @returns {number}
+ */
+const handleSymbol = (multiplyPos, lines, numberStartCol, numberEndCol, row) => {
+    if (numberStartCol === -1)
+        return 0;
+    const [r, c] = getSpecialNodePosition(lines, numberStartCol, numberEndCol, row);
+    //if node pos is invalid
+    if (r === -1 || c === -1)
+        return 0;
+
+    //if it's not a gear
+    if (lines[r][c] !== '*')
+        return 0;
+
+    const num = Number(lines[row].substring(numberStartCol, numberEndCol + 1));
+    //keys are strings bcs array as a key doesnt work as expected
+    const key = `${r},${c}`;
+    if (multiplyPos.has(key))
+    {
+        const toReturn = num * multiplyPos.get(key);
+        multiplyPos.delete(key);
+        return toReturn;
+    }
+    multiplyPos.set(key, num); 
+    return 0;
+}
+
+/**
+ * 
+ * @param {string} path 
+ * @returns {number}
+ */
+const part2 = (path) => {
+    const lines = loadLines(path);
+
+    let sum = 0;
+    const multiplyPos = new Map();
+    for (let row = 0; row < lines.length; row++)
+    {
+        let numberStartCol = -1;
+        for (let col = 0; col < lines[row].length; col++)
+        {
+            if (isNum(lines[row][col]))
+            { 
+                if (numberStartCol === -1)
+                    numberStartCol = col;
+            }
+            else {
+                sum += handleSymbol(multiplyPos, lines, numberStartCol, col - 1, row);         
+                numberStartCol = -1
+            }
+        }
+        sum += handleSymbol(multiplyPos, lines, numberStartCol, lines[row].length - 1, row);         
+        numberStartCol = -1
+    }
+    return sum;
+}
+
+module.exports = {part1, part2, checkIfNumIsValid}
